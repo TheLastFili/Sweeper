90ca211a0148de3c6db1b54b867978e070a38db1
TheLastFili <143823718+TheLastFili@users.noreply.github.com>
2025-02-19 22:21:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO
 import datetime
 from collections import defaultdict
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 # Using defaultdict to automatically initialize empty lists for new dates
 timers_by_date = defaultdict(list)
@@ -15,7 +13,6 @@ def index():
 
 @app.route('/api/timers/<date>')
 def get_timers(date):
-    # Return empty list if no timers exist for the date
     return jsonify(timers_by_date[date])
 
 @app.route('/api/timer', methods=['POST'])
@@ -23,16 +20,14 @@ def save_timer():
     data = request.get_json()
     date = data['date']
     timer = data['timer']
-
-    # If timer has an index, update existing timer
+    
     if 'index' in data:
         index = data['index']
         if 0 <= index < len(timers_by_date[date]):
             timers_by_date[date][index] = timer
     else:
-        # Otherwise append new timer
         timers_by_date[date].append(timer)
-
+    
     return jsonify({"message": "Timer saved"})
 
 @app.route('/api/timer', methods=['DELETE'])
@@ -40,7 +35,7 @@ def delete_timer():
     data = request.get_json()
     date = data['date']
     index = data['index']
-
+    
     if 0 <= index < len(timers_by_date[date]):
         timers_by_date[date].pop(index)
         return jsonify({"message": "Timer deleted"})
@@ -51,7 +46,7 @@ def start_timer():
     data = request.get_json()
     date = data['date']
     index = data['index']
-
+    
     if 0 <= index < len(timers_by_date[date]):
         timers_by_date[date][index]['start'] = datetime.datetime.now().timestamp() * 1000
         timers_by_date[date][index]['lastStarted'] = datetime.datetime.now().timestamp() * 1000
@@ -63,7 +58,7 @@ def stop_timer():
     data = request.get_json()
     date = data['date']
     index = data['index']
-
+    
     if 0 <= index < len(timers_by_date[date]):
         timer = timers_by_date[date][index]
         if timer['start']:
@@ -78,7 +73,7 @@ def copy_timer():
     data = request.get_json()
     target_date = data['targetDate']
     timer = data['timer']
-
+    
     timers_by_date[target_date].append(timer)
     return jsonify({"message": "Timer copied"})
 
@@ -87,4 +82,4 @@ def get_report():
     return jsonify(timers_by_date)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
